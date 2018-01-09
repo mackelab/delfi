ba4f27e00aa95dfdb653903421c1d5ed358e167e
kaandocal <kaandocal@gmail.com>
2018-01-09 21:28:02 +0100

    Removed invalid imports

--- delfi/simulator/__init__.py
+++ delfi/simulator/__init__.py
@@ -1,4 +1,2 @@
 from delfi.simulator.Gauss import Gauss
 from delfi.simulator.GaussMixture import GaussMixture
-from delfi.simulator.BaseSimulator import BaseSimulator
-from delfi.simulator.MaskedSimulator import MaskedSimulator
