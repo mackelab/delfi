c5aeea7dd5f07b7bbb412c9b7a54936c926faf8e
jan-matthis <mail@jan-matthis.de>
2018-01-10 13:27:03 +0100

    formatting changes

--- delfi/distribution/MixedDistribution.py
+++ delfi/distribution/MixedDistribution.py
@@ -2,10 +2,11 @@ import numpy as np
 
 from .BaseDistribution import BaseDistribution
 
-class MixedDistribution(BaseDistribution):
-    """ Mixed distribution (not to be confused with mixture distributions)
 
-    This class defines a concatenation of a list of distributions. It supports eval() (without the ii option) and gen().
+class MixedDistribution(BaseDistribution):
+    """Mixed distribution (not to be confused with mixture distributions)
+
+    This class defines a concatenation of a list of distributions. It supports `eval()` (without the ii option) and `gen()`.
 
     Parameters
     ----------
