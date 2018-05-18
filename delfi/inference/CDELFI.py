5dd3802c459b09399b884d25b2e936ffe539fb88
mnonnenm <marcel.nonnenmacher@caesar.de>
2018-05-18 14:45:55 +0200

    Update CDELFI.py
    pruning negligible MoG components before attempting to divide posterior estimate by proposal

--- delfi/inference/CDELFI.py
+++ delfi/inference/CDELFI.py
@@ -182,7 +182,7 @@ class CDELFI(BaseInference):
 
         return logs, trn_datasets, posteriors
 
-    def predict(self, x):
+    def predict(self, x, threshold=0.05):
         """Predict posterior given x
 
         Parameters
@@ -197,6 +197,8 @@ class CDELFI(BaseInference):
             # mog is posterior given proposal prior
             mog = super(CDELFI, self).predict(x)  # via super
 
+            mog.prune_negligible_components(threshold=threshold)
+            
             # compute posterior given prior by analytical division step
             if 'Uniform' in str(type(self.generator.prior)):
                 posterior = mog / self.generator.proposal
