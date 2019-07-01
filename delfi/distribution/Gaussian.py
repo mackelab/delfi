5a86ca276a1d7e37dacd11e7244882822819a726
David Greenberg <david.s.greenberg@gmail.com>
2019-07-01 15:21:58 +0200

    fix error for Gaussian marginals

--- delfi/distribution/Gaussian.py
+++ delfi/distribution/Gaussian.py
@@ -209,17 +209,19 @@ class Gaussian(BaseDistribution):
     def eval(self, x, ii=None, log=True):
         # See BaseDistribution.py for docstring
         x = np.atleast_2d(x)
-        assert x.shape[1] == self.ndim, "incorrect data dimension"
 
         if ii is None:
+            assert x.shape[1] == self.ndim, "incorrect data dimension"
             xm = x - self.m
             lp = -np.sum(np.dot(xm, self.P) * xm, axis=1)
             lp += self.logdetP - self.ndim * np.log(2.0 * np.pi)
             lp *= 0.5
 
         else:
+            ii = np.atleast_1d(ii)
             m = self.m[ii]
             S = self.S[ii][:, ii]
+            assert x.shape[1] == m.size
             if np.linalg.matrix_rank(S)==len(S[:,0]):
                 lp = scipy.stats.multivariate_normal.logpdf(x, m, S, allow_singular=True)
                 lp = np.array([lp]) if x.shape[0] == 1 else lp
