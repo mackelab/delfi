eb6e6ae6f636b3c88719f63173bcea4986e023b5
David Greenberg <david.s.greenberg@gmail.com>
2019-07-04 15:58:34 +0200

    finished transformedsim, added test

--- delfi/simulator/TransformedSimulator.py
+++ delfi/simulator/TransformedSimulator.py
@@ -1,16 +1,19 @@
-import numpy as np
+from copy import deepcopy
 
 from delfi.simulator.BaseSimulator import BaseSimulator
 
 
 class TransformedSimulator(BaseSimulator):
-    def __init__(self, simulator, inverse_bijection):
+    def __init__(self, simulator, inverse_bijection, makecopy=False):
         '''
-        Simulator with parameters in a transformed
+        Simulator with parameters in a transformed space
 
         :param simulator: Original simulator
         :param inverse_bijection: Inverse transformation back into original simulator's parameter space
+        :param makecopy: Whether to call deepcopy on the simulator, unlinking the RNGs
         '''
+        if makecopy:
+            simulator = deepcopy(simulator)
         self.simulator, self.inverse_bijection = simulator, inverse_bijection
         self.dim_param = self.simulator.dim_param
 
