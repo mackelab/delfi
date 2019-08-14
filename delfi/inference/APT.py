260229a7cb0dd41e0fa43b273c0fefffc9b5a638
David Greenberg <david.s.greenberg@gmail.com>
2019-08-14 18:56:26 +0200

    add patience and monitor_every as named inputs to all APT run methods, and pass them to the trainer's train method

--- delfi/inference/APT.py
+++ delfi/inference/APT.py
@@ -252,13 +252,13 @@ class APT(BaseInference):
                     **kwargs)
         log = t.train(epochs=self.epochs_round(epochs), minibatch=minibatch,
                       verbose=verbose, print_each_epoch=print_each_epoch,
-                      stop_on_nan=stop_on_nan, patience=patience,
-                      monitor_every=monitor_every)
+                      stop_on_nan=stop_on_nan, patience=patience, monitor_every=monitor_every)
 
         return log, trn_data
 
     def run_gaussian(self, n_train=100, epochs=100, minibatch=50, n_atoms=None, moo=None,  train_on_all=False,
                      round_cl=1, stop_on_nan=False, monitor=None, verbose=False, print_each_epoch=False,
+                     patience=20, monitor_every=None,
                      reuse_prior_samples=True, **kwargs):
 
         # simulate data
@@ -311,12 +311,14 @@ class APT(BaseInference):
                     **kwargs)
 
         log = t.train(epochs=self.epochs_round(epochs), minibatch=minibatch, verbose=verbose,
-                      print_each_epoch=print_each_epoch, stop_on_nan=stop_on_nan)
+                      print_each_epoch=print_each_epoch, stop_on_nan=stop_on_nan,
+                      patience=patience, monitor_every=monitor_every)
 
         return log, trn_data
 
     def run_atomic(self, n_train=100, epochs=100, minibatch=50, n_atoms=10, moo='resample', train_on_all=False,
                    reuse_prior_samples=True, combined_loss=False, round_cl=1, stop_on_nan=False, monitor=None,
+                   patience=20, monitor_every=None,
                    verbose=False, print_each_epoch=False, **kwargs):
 
         # activetrainer doesn't de-norm params before evaluating the prior
@@ -363,13 +365,14 @@ class APT(BaseInference):
                           **kwargs)
 
         log = t.train(epochs=self.epochs_round(epochs), minibatch=minibatch, verbose=verbose,
-                      print_each_epoch=print_each_epoch, strict_batch_size=True)
+                      print_each_epoch=print_each_epoch, strict_batch_size=True,
+                      patience=patience, monitor_every=monitor_every)
 
         return log, trn_data
 
     def run_MoG(self, n_train=100, epochs=100, minibatch=50, n_atoms=None, moo=None, train_on_all=False, round_cl=1,
                 stop_on_nan=False, monitor=None, verbose=False, print_each_epoch=False, reuse_prior_samples=True,
-                **kwargs):
+                patience=20, monitor_every=None, **kwargs):
         assert not train_on_all, "train_on_all is not yet implemented for MoG "\
                 "proposals"
 
@@ -408,7 +411,8 @@ class APT(BaseInference):
                     **kwargs)
 
         log = t.train(epochs=self.epochs_round(epochs), minibatch=minibatch, verbose=verbose,
-                      print_each_epoch=print_each_epoch, stop_on_nan=stop_on_nan)
+                      print_each_epoch=print_each_epoch, stop_on_nan=stop_on_nan,
+                      patience=patience, monitor_every=monitor_every)
 
         return log, trn_data
 
