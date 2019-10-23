add prior precision to posterior comp(s) for mog/gauss prop, numerical fixes for MoG prop, reuse prior samples and train on all rounds in MoG prop test

delfi/inference/APT.py
@@ -14,6 +14,7 @@ from delfi.utils.data import repnewax, combine_trn_datasets
 class APT(BaseInference):
     def __init__(self, generator, obs=None, prior_norm=False,
                  pilot_samples=100, reg_lambda=0.01, seed=None, verbose=True,
+                 add_prior_precision=True,
                  **kwargs):
         """APT
         Core idea is to parameterize the true posterior, and calculate the
@@ -40,6 +41,8 @@ class APT(BaseInference):
             If provided, random number generator will be seeded
         verbose : bool
             Controls whether or not progressbars are shown
+        add_prior_precision: bool
+            Whether to add the prior precision to each posterior compoenent for Gauss/MoG proposals
         kwargs : additional keyword arguments
             Additional arguments for the NeuralNet instance, including:
                 n_hiddens : list of ints
@@ -66,9 +69,21 @@ class APT(BaseInference):
         if np.any(np.isnan(self.obs)):
             raise ValueError("Observed data contains NaNs")
 
+        self.add_prior_precision = add_prior_precision
         self.reg_lambda = reg_lambda
         self.exception_info = (None, None, None)
         self.trn_datasets, self.proposal_used = [], []
+
+    def predict(self, *args, **kwargs):
+        p = super().predict(*args, **kwargs)
+
+        # add the prior precision to each posterior component if needed
+        if self.add_prior_precision and self.round > 0 and isinstance(self.generator.prior, dd.Gaussian) and \
+                self.proposal_used[-1] in ['gaussian', 'mog']:
+            assert self.network.density == 'mog' and isinstance(p, dd.MoG)
+            p = dd.MoG(a=p.a, xs=[dd.Gaussian(m=x.m, P=x.P + self.generator.prior.P, seed=x.seed) for x in p.xs])
+
+        return p
 
     def define_loss(self, n, round_cl=1, proposal='gaussian',
                     combined_loss=False):
@@ -91,18 +106,18 @@ class APT(BaseInference):
             prior = prior.ztrans(self.params_mean, self.params_std)
 
         if proposal == 'prior':  # using prior as proposal
-            loss, trn_inputs = snpe_loss_prior_as_proposal(self.network,
-                                                           svi=self.svi)
+            loss, trn_inputs = snpe_loss_prior_as_proposal(self.network, svi=self.svi)
+
         elif proposal == 'gaussian':
+            assert self.network.density == 'mog'
             assert isinstance(self.generator.proposal, dd.Gaussian)
-            loss, trn_inputs = apt_loss_gaussian_proposal(self.network,
-                                                          prior,
-                                                          svi=self.svi)
+            loss, trn_inputs = apt_loss_gaussian_proposal(self.network, prior, svi=self.svi,
+                                                          add_prior_precision=self.add_prior_precision)
         elif proposal.lower() == 'mog':
+            assert self.network.density == 'mog'
             assert isinstance(self.generator.proposal, dd.MoG)
-            loss, trn_inputs = apt_loss_MoG_proposal(self.network,
-                                                     prior,
-                                                     svi=self.svi)
+            loss, trn_inputs = apt_loss_MoG_proposal(self.network, prior, svi=self.svi,
+                                                     add_prior_precision=self.add_prior_precision)
         elif proposal == 'atomic':
             loss, trn_inputs = \
                 apt_loss_atomic_proposal(self.network, svi=self.svi,
@@ -330,10 +345,9 @@ class APT(BaseInference):
         self.set_proposal(project_to_gaussian=False)
         assert isinstance(self.generator.proposal, dd.MoG)
         prop = self.generator.proposal.ztrans(self.params_mean, self.params_std)
-        ncomps = prop.n_components
 
         trn_data, n_train_round = self.gen(n_train)
-        trn_data = (*trn_data, *MoG_prop_APT_training_vars(prop, n_train_round, ncomps))
+        trn_data = (*trn_data, *MoG_prop_APT_training_vars(prop, n_train_round, prop.n_components))
 
         self.trn_datasets.append(trn_data)
 
@@ -469,7 +483,6 @@ class APT(BaseInference):
         n_train_round = trn_data[0].shape[0]  # may have decreased (rejection)
 
         return trn_data, n_train_round
-
 
     def n_train_round(self, n_train):
         # number of training examples for this round

delfi/neuralnet/loss/lossfunc.py
@@ -42,7 +42,7 @@ def snpeb_loss(model, svi=False):
     return loss, trn_inputs
 
 
-def apt_loss_MoG_proposal(mdn, prior, n_proposal_components=None, svi=False):
+def apt_loss_MoG_proposal(mdn, prior, n_proposal_components=None, svi=False, add_prior_precision=True):
     """Define loss function for training with a MoG proposal, allowing
     the proposal distribution to be different for each sample. The proposal
     means, precisions and weights are passed along with the stats and params.
@@ -81,6 +81,7 @@ def apt_loss_MoG_proposal(mdn, prior, n_proposal_components=None, svi=False):
         mdn.get_mog_tensors(return_extras=True, svi=svi)
     # convert mixture vars from lists of tensors to single tensors, of sizes:
     las = tt.log(a)
+    Us = tt.stack(Us, axis=3).dimshuffle(0, 3, 1, 2)
     Ps = tt.stack(Ps, axis=3).dimshuffle(0, 3, 1, 2)
     Pms = tt.stack(Pms, axis=2).dimshuffle(0, 2, 1)
     ldetPs = tt.stack(ldetPs, axis=1)
@@ -106,7 +107,7 @@ def apt_loss_MoG_proposal(mdn, prior, n_proposal_components=None, svi=False):
     # calculate corrections to precisions (P_0s) and precisions * means (Pm_0s)
     P_0s = prop_Ps
     Pm_0s = prop_Pms
-    if not uniform_prior:  # Gaussian prior
+    if not uniform_prior and not add_prior_precision:  # Gaussian prior
         P_0s = P_0s - prior.P
         Pm_0s = Pm_0s - prior.Pm
 
@@ -116,14 +117,36 @@ def apt_loss_MoG_proposal(mdn, prior, n_proposal_components=None, svi=False):
     # component of the true posterior appear first. The shape of pp_Ps is
     # (batch, mdn.n_components, ncprop, n_outputs, n_outputs)
     pp_Ps = Ps.dimshuffle(0, 1, 'x', 2, 3) + P_0s.dimshuffle(0, 'x', 1, 2, 3)
-    pp_Ss = invert_each(pp_Ps)  # covariances of proposal posterior components
-    # upper Cholesky factors for precisions of proposal posterior components
-    pp_Us = cholesky_each(pp_Ps).dimshuffle(0, 1, 2, 4, 3)  # cholesky_each returns lower factor
-    # log determinants of upper Cholesky factor for the precision of each proposal posterior component
-    pp_ldetUs = tt.sum(tt.log(tt.sum(pp_Us * np.eye(mdn.n_outputs), axis=4)), axis=3)
-    # log determinant of each proposal posterior component's precision:
-    pp_ldetPs = 2.0 * pp_ldetUs
+    spp = pp_Ps.shape
 
+    # get square roots of diagonal entries of posterior proposal precision components, which are equal to the L2 norms
+    # of the Cholesky factor columns for the same matrix. we'll use these to improve the numerical conditioning of pp_Ps
+    ds = tt.sqrt(tt.sum(pp_Ps * np.eye(mdn.n_outputs), axis=4))
+    Ds = ds.dimshuffle(0, 1, 2, 'x', 3) * ds.dimshuffle(0, 1, 2, 3, 'x')
+    # normalize the estimate of each true posterior component according to the corresponding elements of d:
+    # first normalize the Cholesky factor of the true posterior component estimate, separately for each pairing of
+    # proposal and true posterior components...
+    Us_normed = Us.dimshuffle(0, 1, 'x', 2, 3) / ds.dimshuffle(0, 1, 2, 'x', 3)
+    # then normalize the propsal. the resulting list is the same proposal, differently normalized for each component of
+    # the true posterior
+    P_0s_normed = P_0s.dimshuffle(0, 'x', 1, 2, 3) / Ds
+
+    # reshape temporarily so there's just one batch dimension
+    Us_normed_R = Us_normed.reshape((-1, mdn.n_outputs, mdn.n_outputs))
+    Ps_normed_R = tt.batched_dot(Us_normed_R.dimshuffle(0, 2, 1), Us_normed_R)
+    Ps_normed = Ps_normed_R.reshape(spp)
+
+    pp_Ps_normed = Ps_normed + P_0s_normed
+    # lower Cholesky factors for normalized precisions of proposal posterior components
+    pp_Ls_normed = cholesky_each(pp_Ps_normed)
+    # log determinants of lower Cholesky factors for normalized precisions of proposal posterior components
+    pp_ldetLs_normed = tt.sum(tt.log(tt.sum(pp_Ls_normed * np.eye(mdn.n_outputs), axis=4)), axis=3)
+    # precisions of proposal posterior components (now well-conditioned)
+    pp_Ps = pp_Ps_normed * Ds
+    # log determinants of proposal posterior precisions
+    pp_ldetPs = 2.0 * (tt.sum(tt.log(ds), axis=3) + pp_ldetLs_normed)
+    # covariance matrices
+    pp_Ss = invert_each(pp_Ps)  # covariances of proposal posterior components
     # precision times mean for each proposal posterior component:
     pp_Pms = Pms.dimshuffle(0, 1, 'x', 2) + Pm_0s.dimshuffle(0, 'x', 1, 2)
     # mean of proposal posterior components:
@@ -175,7 +198,7 @@ def apt_loss_MoG_proposal(mdn, prior, n_proposal_components=None, svi=False):
     return loss, trn_inputs
 
 
-def apt_loss_gaussian_proposal(mdn, prior, svi=False):
+def apt_loss_gaussian_proposal(mdn, prior, svi=False, add_prior_precision=True):
     """Define loss function for training with a Gaussian proposal, allowing
     the proposal distribution to be different for each sample. The proposal
     mean and precision are passed along with the stats and params.
@@ -220,7 +243,7 @@ def apt_loss_gaussian_proposal(mdn, prior, svi=False):
     # calculate corrections to precision (P_0) and precision * mean (Pm_0)
     P_0 = prop_P
     Pm_0 = tt.sum(prop_P * prop_m.dimshuffle(0, 'x', 1), axis=2)
-    if not uniform_prior:  # Gaussian prior
+    if not uniform_prior and not add_prior_precision:  # Gaussian prior
         P_0 = P_0 - prior.P
         Pm_0 = Pm_0 - prior.Pm
 