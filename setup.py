1ef24d5cc3330db2348a681a86d256681e90e321
Jan-Matthis <mail@jan-matthis.de>
2020-02-15 17:59:27 +0100

    Remove dependency_links (deprecated)

--- setup.py
+++ setup.py
@@ -1,22 +1,34 @@
 from setuptools import setup
 
-exec(open('delfi/version.py').read())
+exec(open("delfi/version.py").read())
 
 setup(
-    name='delfi',
+    name="delfi",
     version=__version__,
-    description='Density estimation likelihood-free inference',
-    url='https://github.com/mackelab/delfi',
-    author='Mackelab',
-    packages=['delfi', 'delfi.distribution', 'delfi.distribution.mixture',
-              'delfi.generator', 'delfi.inference',
-              'delfi.neuralnet', 'delfi.neuralnet.layers',
-              'delfi.neuralnet.loss', 'delfi.simulator', 'delfi.summarystats',
-              'delfi.utils'],
-    license='BSD',
-    install_requires=['dill', 'lasagne==0.2.dev1', 'numpy', 'scipy', 'theano', 'tqdm', 'snl'],
-    dependency_links=[
-        'https://github.com/Lasagne/Lasagne/archive/master.zip#egg=lasagne-0.2.dev1',
-        'https://github.com/mnonnenm/SNL_py3port',
-    ]
+    description="Density estimation likelihood-free inference",
+    url="https://github.com/mackelab/delfi",
+    author="Mackelab",
+    packages=[
+        "delfi",
+        "delfi.distribution",
+        "delfi.distribution.mixture",
+        "delfi.generator",
+        "delfi.inference",
+        "delfi.neuralnet",
+        "delfi.neuralnet.layers",
+        "delfi.neuralnet.loss",
+        "delfi.simulator",
+        "delfi.summarystats",
+        "delfi.utils",
+    ],
+    license="BSD",
+    install_requires=[
+        "dill",
+        "lasagne@git+https://github.com/Lasagne/Lasagne#egg=lasagne",
+        "numpy",
+        "scipy",
+        "theano",
+        "tqdm",
+        "snl@git+https://github.com/mackelab/snl#egg=snl",
+    ],
 )
